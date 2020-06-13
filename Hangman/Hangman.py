eea6ef7cb7780ba7e5fa1e9b54b6cf7df0d50e1b
Franklin Abreu <abreufranklin589@gmail.com>
2020-06-13 00:02:57 -0400

    update hangman

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,7 +1,12 @@
+import random
+
 print ("H A N G M A N\nThe game will be available soon.")
 
 word_list = ["python", "java", "kotlin", "javascript"]
+rand_word = random.choice(word_list)
+n_dashes = "-" * (len(rand_word) - 3)
+hidden_word = rand_word[0:3] + n_dashes
 
-print("Guess the word: ")
+print(f"Guess the word {hidden_word}: ")
 word = input()
-print("You survived!" if word == "python" else "You are hanged!")
+print("You survived!" if word == rand_word else "You are hanged!")
