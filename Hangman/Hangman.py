1cab9be2bb510edee6ee5f3132f280e293145d23
Franklin Abreu <abreufranklin589@gmail.com>
2021-01-19 20:16:10 -0500

    upload most recent

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,16 +1,17 @@
 import random
 
-print ("H A N G M A N")
+print("H A N G M A N")
 
 word_list = ["python", "java", "kotlin", "javascript"]
 rand_word = random.choice(word_list)
 hidden_word = "-" * (len(rand_word))
 hidden_list = list(hidden_word)
-words_attempted= []
+words_attempted = []
+
 
 def game():
-    num = 8 # number of tries
-    
+    num = 8  # number of tries
+
     while num > 0:
         print("")
         tring = "".join(hidden_list)
@@ -18,7 +19,7 @@ def game():
         if tring == rand_word:
             print("You guessed the word!\nYou survived!")
             break
-    
+
         letter = input("Input a letter: ")
 
         if len(letter) != 1:
@@ -28,16 +29,16 @@ def game():
         elif letter in words_attempted:
             print("You already typed this letter")
         elif letter.lower() in rand_word:
-            for j in range(len(rand_word)):           
+            for j in range(len(rand_word)):
                 if letter == rand_word[j]:
-                    hidden_list[j] = letter             
+                    hidden_list[j] = letter
         else:
             print("No such letter in the word")
-            num -= 1  
+            num -= 1
         words_attempted.append(letter)
     if num == 0:
         print("You are hanged!")
-        
+
 
 while True:
     start_end = input('Type "play" to play the game, "exit" to quit: ')
--- MatrixProcessor/MatrixProcessor.py
+++ MatrixProcessor/MatrixProcessor.py
@@ -1,23 +1,49 @@
-
-def matrix_add():
-    matrix_a = []
-    matrix_b = []
-    
-    size_a = input("Enter size of first matrix:")
-    row_a,col_a = size_a.split(" ")
-    print("Enter first matrix:")
-    for i in range(int(row_a)):
+def read_matrix(rows):
+    matrix = []
+    for _i in range(int(rows)):
         matrix_data = input().split(" ")
-        matrix_a.append(matrix_data)
-    
+        matrix.append(matrix_data)
+    return matrix
+
+
+def options(func):
+    # Might delete later
+    if func == 1:
+        print("""1. Add matrices
+2. Multiply matrix by a constant
+3. Multiply matrices
+4. Transpose matrix
+0. Exit""")
+
+    elif func == 2:
+        print("""1. Main Diagonal
+2. Side Diagonal
+3. Vertical Line
+4. Horizontal Line""")
+
+    choice = input("Your choice:")
+
+    return choice
+
+
+def print_matrix(output):
+    print("The result is:\n")
+    for i in output:
+        print(" ".join(str(x) for x in i))
+
+
+def adder():
+    size_a = input("Enter size of first matrix:")
+    row_a, col_a = size_a.split(" ")
+    print("Enter first matrix:")
+    matrix_a = read_matrix(row_a)
+
     size_b = input("Enter size of second matrix:")
-    row_b,col_b = size_b.split(" ")
-    col_b *= 0 # delete later
+    row_b, col_b = size_b.split(" ")
+    col_b *= 0  # delete later
     print("Enter second matrix:")
     if size_a == size_b:
-        for i in range(int(row_b)):
-            matrix_data = input().split(" ")
-            matrix_b.append(matrix_data)
+        matrix_b = read_matrix(row_b)
 
         output_matrix = []
         for i in range(int(row_a)):
@@ -28,55 +54,74 @@ def matrix_add():
                 sum_matrix = a_num + b_num
                 new_var.append(sum_matrix)
             output_matrix.append(new_var)
-        print("The result is:\n")
-        for i in output_matrix:
-            print (" ".join(str(x) for x in i))
+        print_matrix(output_matrix)
     else:
-        print("ERROR")
+        print("The operation cannot be performed.")
 
-def matrix_mul_constant():
-    matrix_a = []
+
+def mul_constant():
+    size_a = input("Enter size of matrix:")
+    row_a, col_a = size_a.split(" ")
+    print("Enter matrix:")
+    matrix_a = read_matrix(row_a)
+
+    const = input("Enter constant:")
+
     output_matrix = []
-
-    size_a = input("Enter size of first matrix:")
-    row_a,col_a = size_a.split(" ")
-    print("Enter first matrix:")
-    for i in range(int(row_a)):
-        matrix_data = input().split(" ")
-        matrix_a.append(matrix_data)
-    
-    size_b = input("Enter constant:")
-
     for i in range(int(row_a)):
         placeholder = []
         for j in range(int(col_a)):
-            a_num = int(matrix_a[i][j])
-            mul_var = a_num * int(size_b)
+            a_num = float(matrix_a[i][j])
+            mul_var = a_num * float(const)
             placeholder.append(mul_var)
         output_matrix.append(placeholder)
 
-    print("The result is:")
-    for i in output_matrix:
-        print (" ".join(str(x) for x in i))
+    print_matrix(output_matrix)
 
 
-def matrix_mul():
-    print("soon TM")
+def multiplier():
+    size_a = input("Enter size of first matrix:")
+    row_a, col_a = size_a.split(" ")
+    print("Enter first matrix:")
+    matrix_a = read_matrix(row_a)
 
-while True:
-    print("""1. Add matrices
-2. Multiply matrix by a constant
-3. Multiply matrices
-0. Exit""")
+    size_b = input("Enter size of second matrix:")
+    row_b, col_b = size_b.split(" ")  # pylint: disable=unused-variable
 
-    choice = input("Your choice:")
+    print("Enter second matrix:")
+    if row_b == col_a:
+        matrix_b = read_matrix(row_b)
+
+        output_matrix = [[sum(int(a)*int(b) for a, b in zip(row_a, col_b))
+                          for col_b in zip(*matrix_b)] for row_a in matrix_a]
+
+        print_matrix(output_matrix)
+
+
+def transposer():
+    choice = options(2)
 
     if choice == "1":
-        matrix_add()
+        pass
     elif choice == "2":
-        matrix_mul_constant()
+        pass
     elif choice == "3":
-        matrix_mul()
+        pass
+    elif choice == "4":
+        pass
+
+
+while True:
+    choice = options(1)
+
+    if choice == "1":
+        adder()
+    elif choice == "2":
+        mul_constant()
+    elif choice == "3":
+        multiplier()
+    elif choice == "4":
+        transposer()
     elif choice == "0":
         break
     else:
